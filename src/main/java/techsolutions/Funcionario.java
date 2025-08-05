68789b2924e74d9a20a0932e1f4da00974821c55
Iago Santana <iagosantanaof@gmail.com>
2025-08-05 18:16:16 -0300

    Getters e Setters adicionados e com validações.

--- src/main/java/techsolutions/Funcionario.java
+++ src/main/java/techsolutions/Funcionario.java
@@ -12,6 +12,42 @@ public class Funcionario {
         this.salario = salario;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null) {
+            throw new IllegalArgumentException("Campo vazio!");
+        } else {
+            this.nome = nome;
+        }
+    }
+
+    public void setSalario(double salario) {
+        if (salario < 0) {
+            throw new IllegalArgumentException("Salário incorreto!")
+        } else {
+            this.salario = salario;
+        }
+    }
+
+    public void setIdade(int idade) {
+        if(idade > 0) {
+            this.idade = idade;
+        } else {
+            throw new IllegalArgumentException("A idade não ta correta!");
+        }
+    }
+
     public void exibirInformacoes() {
         System.out.println("Funcionário: " + nome + ", Idade: " + idade + ", Salário: " + salario);
     }
--- src/main/java/techsolutions/Main.java
+++ src/main/java/techsolutions/Main.java
@@ -2,7 +2,9 @@ package techsolutions;
 
 public class Main {
     public static void main(String[] args) {
-        Funcionario funcionario1 = new Funcionario("Iago", 21, 800.00);
+        Funcionario funcionario1 = new Funcionario("d", 0, 800.00);
         funcionario1.exibirInformacoes();
+
+        funcionario1.setIdade(0);
     }
 }
